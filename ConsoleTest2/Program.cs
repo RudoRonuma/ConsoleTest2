00fc96fb3539f6e061c510408003ad1181da5884
Aliwoto <woto@kaizoku.cyou>
2022-12-20 00:08:35 +0330

    Add project files.

---
+++ ConsoleTest2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleTest2ServiceProvider;
+
+namespace ConsoleTest2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string a = "";
+            a.Split("=");
+            Console.WriteLine($"Hello, World from {MyConsoleProvider.GetProvidingString()}!");
+        }
+    }
+}
---
+++ ConsoleTest2ServiceProvider/MyConsoleProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest2ServiceProvider
+{
+    public class MyConsoleProvider
+    {
+        public static string GetProvidingString() =>
+            typeof(MyConsoleProvider).Assembly.GetName().Version!.ToString();
+    }
+}
